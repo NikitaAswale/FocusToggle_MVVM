cadee147716781eeac9d03e5f837a1d8b5b2d8a7
someshsampat <someshsampat1234@gmail.com>
2026-04-28 22:04:29 +0530

    28 apr  change

--- app/src/main/java/com/example/focustoggle_mvvm/FocusToggleUI.kt
+++ app/src/main/java/com/example/focustoggle_mvvm/FocusToggleUI.kt
@@ -52,7 +52,7 @@ fun FocusToggle_UI(viewModel: ToggleViewModel = viewModel()){
     Box(modifier = Modifier.fillMaxSize()){
         Column() {
             Top()
-            Spacer(Modifier.height(20.dp))
+            Spacer(Modifier.height(24.dp))
             Middle(TODOS, viewModel)
         }
         Bottom()
